3f32b6b0b67549150537a9723599ce037c4e4b1a
GitHub Action <action@github.com>
2024-10-14 10:06:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/raschild6/momentumplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/raschild6/momentumplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.raschild6.momentumplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
